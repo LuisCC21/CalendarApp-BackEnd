9523851e69dd14e794b56026d6f1bbbefe22f9f5
Luis <carcamocariasl@yahoo.com>
2022-10-18 21:02:07 -0600

    path * agregado

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.use(express.json()); // todo lo que pase en formato json, pasara por aqui
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/events',require('./routes/events'));
 
+/* app.get('*',(req,res)=>{ //clase 429
+    res.sendFile(__dirname+'/public/index.html');
+}) */
+
 
 
 //Escuchar peticiones
