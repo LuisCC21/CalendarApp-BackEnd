9a49045a41896b2acfa158830ae9268a7cee91ba
Luis <carcamocariasl@yahoo.com>
2023-04-17 20:38:23 -0600

    Agregando Cors

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const express = require('express');
-const dbConnection = require('./database/config');
-require('dotenv').config();
-var cors = require('cors');
+const express = require('express')
+const dbConnection = require('./database/config')
+require('dotenv').config()
+var cors = require('cors')
 
 //Crear el servidor de express
-const app = express();
+const app = express()
 
 //Base de datos
-dbConnection();
+dbConnection()
 
 //CORS
-app.use(cors());
-
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+}
+app.use(cors(corsOptions))
 
 //Directorio Publico
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 //Lectura y parseo del body
-app.use(express.json()); // todo lo que pase en formato json, pasara por aqui
+app.use(express.json()) // todo lo que pase en formato json, pasara por aqui
 
 //Rutas
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/events',require('./routes/events'));
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/events', require('./routes/events'))
 
-app.get('*',(req,res)=>{ //clase 429
-    res.sendFile(__dirname+'/public/index.html');
+app.get('*', (req, res) => {
+  //clase 429
+  res.sendFile(__dirname + '/public/index.html')
 })
 
-
-
 //Escuchar peticiones
-app.listen(process.env.PORT,()=>{
-    console.log(`servidor corriendo en puerto ${process.env.PORT}`);
-
+app.listen(process.env.PORT, () => {
+  console.log(`servidor corriendo en puerto ${process.env.PORT}`)
 })
