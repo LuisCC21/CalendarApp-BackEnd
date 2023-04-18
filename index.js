f252fd0a7fbd666c47f2599771c9bc7c83fdd2d6
Luis <carcamocariasl@yahoo.com>
2023-04-18 11:39:43 -0600

    Configurando Cors

--- index.js
+++ index.js
@@ -9,10 +9,21 @@ const app = express()
 //Base de datos
 dbConnection()
 
-//CORS
+// Configurar CORS
+const whitelist = [process.env.FRONTEND_URL]
+
 const corsOptions = {
-  origin: process.env.FRONTEND_URL,
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      // Puede consultar la API
+      callback(null, true)
+    } else {
+      // No esta permitido
+      callback(new Error('Error de Cors'))
+    }
+  },
 }
+
 app.use(cors(corsOptions))
 
 //Directorio Publico
